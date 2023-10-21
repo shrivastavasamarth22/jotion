363900adbb5c4c73b0e80428294705c08cabec02
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-21 13:06:27 +0530

    Completed the dev environment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <p className="text-red-500 font-bold">Hello Notion Clone</p>
+            <Button>
+                Click!
+            </Button>
         </div>
     );
 }
