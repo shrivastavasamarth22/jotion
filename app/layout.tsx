2b873e85b9709244f5b11a10e6ccb0fc1cb896d5
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-10-22 01:40:01 +0530

    created document schema for convex database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "./globals.css";
 
+import { ConvexClientProvider } from "@/components/providers/convex.provider";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { ConvexClientProvider } from "@/components/providers/convex.provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,6 +43,7 @@ export default function RootLayout({
                         disableTransitionOnChange
                         storageKey="jotion-theme-2"
                     >
+                        <Toaster position="bottom-center" />
                         {children}
                     </ThemeProvider>
                 </ConvexClientProvider>
