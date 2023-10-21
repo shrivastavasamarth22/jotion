50ac2b6d485bc06380f6d978b73eb9bcf21c62b6
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-21 17:59:51 +0530

    Set up clerk and convex and made a ClerkConvex Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex.provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,15 +34,17 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                    storageKey="jotion-theme-2"
-                >
-                    {children}
-                </ThemeProvider>
+                <ConvexClientProvider>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                        storageKey="jotion-theme-2"
+                    >
+                        {children}
+                    </ThemeProvider>
+                </ConvexClientProvider>
             </body>
         </html>
     );
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: 'https://credible-marten-71.clerk.accounts.dev',
+            applicationID: "convex"
+        }
+    ]
+};
