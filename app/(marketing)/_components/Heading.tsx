b5ef9f41160ec947ec0a0a878c65f0eeda31679b
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-21 15:15:28 +0530

    Created marketing page with Heading, Hero and Footer components

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,25 +1,27 @@
-"use client"
+"use client";
 
-import { ArrowRight } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Heading = () => {
-  return (
-    <div className='max-w-3xl space-y-4'>
-        <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
-            Your Ideas, Documents, & Plans. Unified. Welcome to <span className='underline'>Jotion</span>
-        </h1>
-        <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
-            Jotion is the connected workspace where <br />
-            better, faster work happens.
-        </h3>
-        <Button>
-            Enter Jotion
-            <ArrowRight className='h-4 w-4 ml-2' />
-        </Button>
-    </div>
-  )
-}
+    return (
+        <div className="max-w-3xl space-y-4">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+                Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+                <span className="underline">Motion</span>
+            </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+                Motion is the connected workspace where <br />
+                better, faster work happens.
+            </h3>
 
-export default Heading
+            <Button>
+                Enter Motion
+                <ArrowRight className="h-4 w-4 ml-2" />
+            </Button>
+        </div>
+    );
+};
+
+export default Heading;
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Jotion',
+  title: 'Motion',
   description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
