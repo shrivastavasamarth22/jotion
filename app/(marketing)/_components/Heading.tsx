add7e3582e02241b4724b939a6fd65c8e11ff520
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-21 18:51:54 +0530

    Implemented clerk authentication

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -3,8 +3,14 @@
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react"
+import Link from "next/link";
 
 const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -15,11 +21,27 @@ const Heading = () => {
                 Jotion is the connected workspace where <br />
                 better, faster work happens.
             </h3>
-
-            <Button>
-                Enter Jotion
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size={"lg"} />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Jotion
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Jotion free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     );
 };
