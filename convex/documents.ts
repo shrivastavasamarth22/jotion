080b826c8a67eae1af87876921ac8ffaab5c74e2
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-10-22 12:27:30 +0530

    Added a recursive api for archiving notes

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,57 @@ import { mutation, query } from "./_generated/server";
 
 import { v } from "convex/values";
 
+export const archive = mutation({
+    args: {
+        id: v.id("documents")
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if (!existingDocument) {
+            throw new Error("Document not found");
+        }
+
+        if (existingDocument.userId !== userId) {
+            throw new Error("Unauthorised");
+        }
+
+        const recursiveArchive = async (documentId: Id<"documents">) => {
+            const children = await ctx.db
+                .query("documents")
+                .withIndex("by_user_parent", (q) =>
+                    q.eq("userId", userId).eq("parentDocument", documentId)
+                )
+                .collect();
+
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchived: true
+                });
+
+                await recursiveArchive(child._id);
+            }
+        }
+
+        const document = await ctx.db.patch(args.id, {
+            isArchived: true
+        });
+
+        await recursiveArchive(args.id);
+
+
+        return document;
+    }
+})
+
 export const getSidebar = query({
     args: {
         parentDocument: v.optional(v.id("documents")),
