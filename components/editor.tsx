f9529538b5c523384f86359372b7b05f36e7deb9
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-10-23 23:15:43 +0530

    Added local file upload functionality to the editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -5,6 +5,7 @@ import "@blocknote/core/style.css";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 
+import { useEdgeStore } from "@/lib/edgestore";
 import { useTheme } from "next-themes";
 
 interface EditorProps {
@@ -15,6 +16,15 @@ interface EditorProps {
 
 export const Editor = ({ initialContent, onChange, editable }: EditorProps) => {
     const { resolvedTheme } = useTheme();
+    const {edgestore} = useEdgeStore();
+
+    const handleUpload = async (file: File) => {
+        const response = await edgestore.publicFiles.upload({
+            file
+        })
+
+        return response.url
+    }
 
     const editor: BlockNoteEditor = useBlockNote({
         editable,
@@ -24,6 +34,7 @@ export const Editor = ({ initialContent, onChange, editable }: EditorProps) => {
         onEditorContentChange: (editor) => {
             onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
         },
+        uploadFile: handleUpload
     });
 
     return (
