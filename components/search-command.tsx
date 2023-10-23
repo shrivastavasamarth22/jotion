b76a46cb1a01731c2408f48c4c6b63f6519c3926
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-10-23 13:24:22 +0530

    Made a universal search command component

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -20,5 +20,73 @@ import { useUser } from '@clerk/clerk-react'
 export const SearchCommand = () => {
     const { user } = useUser();
     const router = useRouter();
-    const documets = useQuery(api.documents.getSearch)
+    const documents = useQuery(api.documents.getSearch)
+    const [isMounted, setIsMounted] = useState(false)
+
+    const toggle = useSearch(store => store.toggle)
+    const isOpen = useSearch(store => store.isOpen)
+    const onClose = useSearch(store => store.onClose)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault();
+                toggle();
+            }
+        }
+
+        document.addEventListener('keydown', down)
+
+        return () => {
+            document.removeEventListener('keydown', down)
+        }
+    }, [toggle])
+
+
+    const onSelect = (id: string) => {
+        router.push(`/documents/${id}`)
+        onClose();
+    }
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <CommandDialog open={isOpen} onOpenChange={onClose}>
+            <CommandInput 
+                placeholder={`Search ${user?.firstName}'s Jotion`}
+            />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Documents">
+                    {documents?.map(doc => (
+                        <CommandItem
+                            key={doc._id}
+                            value={`${doc._id}-${doc.title}`}
+                            title={doc.title}
+                            onSelect={onSelect}
+                        >
+                            {doc.icon ? (
+                                <p className='mr-2 text-[18px]'>
+                                    {doc.icon}
+                                </p>
+                            ): (
+                                <File 
+                                    className='mr-2 h-4 w-4'
+                                />
+                            )}
+                            <span>
+                                {doc.title}
+                            </span>
+                        </CommandItem>
+                    ))}
+                </CommandGroup>
+            </CommandList>
+        </CommandDialog>
+    )
 }
