8d6aba752faef2409670008e28205f541dcecbcf
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-10-24 00:46:35 +0530

    Trying to resolve the preview feature

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 
 export const useOrigin = () => {
     const [mounted, setMounted] = useState(false);
-
-    const origin = typeof window !== "undefined" 
-    && window.location.origin 
-    ? window.location.origin 
-    : "";
+    const origin =
+        typeof window !== "undefined" && window.location.origin
+            ? window.location.origin
+            : "";
 
     useEffect(() => {
         setMounted(true);
-    }, [])
+    }, []);
 
-    if (!mounted) return "";
+    if (!mounted) {
+        return "";
+    }
 
     return origin;
-
-}
+};
