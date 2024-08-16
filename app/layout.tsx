a250c4bec8d70b91a89b7a4b7ed041d7ac04991c
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-08-16 19:39:21 +0530

    renamed convex provider file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 
-import { ConvexClientProvider } from "@/components/providers/convex.provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
@@ -11,48 +11,48 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Jotion",
-    description: "The connected workspace where better, faster work happens.",
-    icons: {
-        icon: [
-            {
-                media: "(prefers-color-scheme: light)",
-                url: "/logo.svg",
-                href: "/logo.svg",
-            },
-            {
-                media: "(prefers-color-scheme: dark)",
-                url: "/logo-dark.svg",
-                href: "/logo-dark.svg",
-            },
-        ],
-    },
+	title: "Jotion",
+	description: "The connected workspace where better, faster work happens.",
+	icons: {
+		icon: [
+			{
+				media: "(prefers-color-scheme: light)",
+				url: "/logo.svg",
+				href: "/logo.svg",
+			},
+			{
+				media: "(prefers-color-scheme: dark)",
+				url: "/logo-dark.svg",
+				href: "/logo-dark.svg",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={inter.className}>
-                <ConvexClientProvider>
-                    <EdgeStoreProvider>
-                        <ThemeProvider
-                            attribute="class"
-                            defaultTheme="system"
-                            enableSystem
-                            disableTransitionOnChange
-                            storageKey="jotion-theme-2"
-                        >
-                            <Toaster position="bottom-center" />
-                            <ModalProvider />
-                            {children}
-                        </ThemeProvider>
-                    </EdgeStoreProvider>
-                </ConvexClientProvider>
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ConvexClientProvider>
+					<EdgeStoreProvider>
+						<ThemeProvider
+							attribute="class"
+							defaultTheme="system"
+							enableSystem
+							disableTransitionOnChange
+							storageKey="jotion-theme-2"
+						>
+							<Toaster position="bottom-center" />
+							<ModalProvider />
+							{children}
+						</ThemeProvider>
+					</EdgeStoreProvider>
+				</ConvexClientProvider>
+			</body>
+		</html>
+	);
 }
