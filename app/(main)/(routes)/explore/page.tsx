fabf87d65b050758d27a59161c036279f9709f99
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2025-07-12 18:41:28 +0530

    added background image for article component

--- app/(main)/(routes)/explore/page.tsx
+++ app/(main)/(routes)/explore/page.tsx
@@ -6,113 +6,101 @@ import { Article } from "../../_components/article";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { PlusCircle } from "lucide-react";
-import { Spinner } from "@/components/spinner";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import { useMediaQuery } from "usehooks-ts";
 import { useRouter } from "next/navigation";
 
 const ExplorePage = () => {
-    const documents = useQuery(api.documents.getPublished)
+	const documents = useQuery(api.documents.getPublished);
 
-    const create = useMutation(api.documents.create);
-    const router = useRouter();
+	const create = useMutation(api.documents.create);
+	const router = useRouter();
 
-    const isMobile = useMediaQuery("(max-width: 768px)");
+	const isMobile = useMediaQuery("(max-width: 768px)");
 
-    const onCreate = () => {
-        const promise = create({ title: "Untitled" })
-            .then((documentId) => router.push(`/documents/${documentId}`))
+	const onCreate = () => {
+		const promise = create({ title: "Untitled" }).then((documentId) =>
+			router.push(`/documents/${documentId}`)
+		);
 
-        toast.promise(promise, {
-            loading: "Creating a new note...",
-            success: "New note created!",
-            error: "Failed to create a new note.",
-        });
-    };
+		toast.promise(promise, {
+			loading: "Creating a new note...",
+			success: "New note created!",
+			error: "Failed to create a new note.",
+		});
+	};
 
-    if (documents === undefined) {
-        if (isMobile) {
-            return (
-                <div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10 gap-y-10">
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                </div>
-            );
-        }
+	if (documents === undefined) {
+		if (isMobile) {
+			return (
+				<div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10 gap-y-10">
+					<Article.Skeleton />
+					<Article.Skeleton />
+					<Article.Skeleton />
+					<Article.Skeleton />
+					<Article.Skeleton />
+				</div>
+			);
+		}
 
-        return (
-            <div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10">
-                <div className="grid grid-cols-2 gap-10 w-full">
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                    <Article.Skeleton />
-                </div>
-            </div>
-        )
-    }
+		return (
+			<div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10">
+				<div className="grid grid-cols-2 gap-10 w-full">
+					<Article.Skeleton />
+					<Article.Skeleton />
+					<Article.Skeleton />
+					<Article.Skeleton />
+				</div>
+			</div>
+		);
+	}
 
-    if (documents.length === 0) {
-        return (
-            <div className="h-full flex flex-col space-y-4">
-                <Image
-                    src="/empty.png"
-                    height="300"
-                    width="300"
-                    alt="Empty"
-                    className="dark:hidden"
-                />
-                <Image
-                    src="/empty-dark.png"
-                    height="300"
-                    width="300"
-                    alt="Empty"
-                    className="dark:block hidden"
-                />
-                <h2 className="text-lg font-medium">
-                    No published notes found!
-                </h2>
-                <Button onClick={onCreate}>
-                    <PlusCircle className="h-4 w-4 mr-2" />
-                    Create a note
-                </Button>
-            </div>
-        );
-    };
+	if (documents.length === 0) {
+		return (
+			<div className="h-full flex flex-col space-y-4">
+				<Image
+					src="/empty.png"
+					height="300"
+					width="300"
+					alt="Empty"
+					className="dark:hidden"
+				/>
+				<Image
+					src="/empty-dark.png"
+					height="300"
+					width="300"
+					alt="Empty"
+					className="dark:block hidden"
+				/>
+				<h2 className="text-lg font-medium">No published notes found!</h2>
+				<Button onClick={onCreate}>
+					<PlusCircle className="h-4 w-4 mr-2" />
+					Create a note
+				</Button>
+			</div>
+		);
+	}
 
-    if (isMobile) {
-        return (
-            <div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10">
-                {
-                    documents.map(doc => (
-                       <Article
-                            key={doc._id}
-                            document={doc}
-                       />
-                    ))
-                }
-            </div>
-        );
-    }
+	if (isMobile) {
+		return (
+			<div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10">
+				{documents.map((doc) => (
+					<Article key={doc._id} document={doc} />
+				))}
+			</div>
+		);
+	}
 
-    return (
-        <div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10">
-            <div className="grid grid-cols-2 gap-10 w-full">
-                {
-                    documents.map(doc => (
-                        <Article
-                            key={doc._id}
-                            document={doc}
-                        />
-                    ))
-                }
-            </div>
-        </div>
-    )
+	return (
+		<div className="dark:bg-[#1f1f1f] flex flex-col items-center px-5 min-h-[100%] pt-20 pb-10">
+			<div className="grid grid-cols-2 gap-10 w-full">
+				{documents.map((doc) => (
+					<Article key={doc._id} document={doc} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default ExplorePage;
--- app/(main)/_components/article.tsx
+++ app/(main)/_components/article.tsx
@@ -9,116 +9,122 @@ import { useMediaQuery } from "usehooks-ts";
 import { useRouter } from "next/navigation";
 
 interface ArticleProps {
-    document: Doc<"documents">;
+	document: Doc<"documents">;
 }
 
 export const Article = ({ document }: ArticleProps) => {
-    const router = useRouter();
+	const router = useRouter();
 
-    const creationDate = new Date(document._creationTime);
+	const creationDate = new Date(document._creationTime);
 
-    const isMobile = useMediaQuery("(max-width: 768px)");
+	const isMobile = useMediaQuery("(max-width: 768px)");
 
-    const useDate = (date: Date) => {
-        const day = date.getDate();
-        const month = date.toLocaleString("default", { month: "short" });
-        const year = date.getFullYear();
-        const hours = String(date.getHours() % 12).padStart(2, "0");
-        const minutes = String(date.getMinutes()).padStart(2, "0");
-        const ampm = date.getHours() >= 12 ? "PM" : "AM";
+	const useDate = (date: Date) => {
+		const day = date.getDate();
+		const month = date.toLocaleString("default", { month: "short" });
+		const year = date.getFullYear();
+		const hours = String(date.getHours() % 12).padStart(2, "0");
+		const minutes = String(date.getMinutes()).padStart(2, "0");
+		const ampm = date.getHours() >= 12 ? "PM" : "AM";
 
-        const dayStr =
-            day +
-            (day === 1 || day === 21 || day === 31
-                ? "st"
-                : day === 2 || day === 22
-                ? "nd"
-                : day === 3 || day === 23
-                ? "rd"
-                : "th");
+		const dayStr =
+			day +
+			(day === 1 || day === 21 || day === 31
+				? "st"
+				: day === 2 || day === 22
+					? "nd"
+					: day === 3 || day === 23
+						? "rd"
+						: "th");
 
-        return `${dayStr} ${month} ${year}, ${hours}:${minutes} ${ampm}`;
-    };
+		return `${dayStr} ${month} ${year}, ${hours}:${minutes} ${ampm}`;
+	};
 
-    return (
-        <div
-            className={cn(
-                "w-full flex justify-between bg-gray-50 dark:bg-muted rounded-lg px-5 shadow-md",
-                document.tags.length > 0 ? "py-4" : "h-28 py-4",
-                isMobile && "mb-10"
-            )}
-        >
-            <div className="flex flex-col w-[60%] justify-center">
-                <h2 className="text-xs text-muted-foreground mb-0.5">
-                    By:{" "}
-                    <Link
-                        href={`/user/${document.userId}`}
-                    >
-                        <span className="font-semibold italic truncate">
-                            {document.userName
-                                ? document.userName
-                                : "Jotion User"}
-                        </span>
-                    </Link>
-                </h2>
-                <h1
-                    role="button"
-                    className="text-2xl font-semibold truncate"
-                    onClick={() => router.push(`/preview/${document._id}`)}
-                >
-                    {document.title}
-                </h1>
-                {document.tags.length > 0 && (
-                    <div className="flex flex-wrap items-center gap-1 mt-2">
-                        {document.tags.length > 0 &&
-                            document.tags.map((tag) => (
-                                <div
-                                    key={tag}
-                                    className="p-1 border border-muted-foreground text-xs font-medium text-muted-foreground flex items-center justify-center rounded-sm max-w-xs"
-                                >
-                                    <span className="truncate">
-                                        {tag.charAt(0).toUpperCase() +
-                                            tag.slice(1)}
-                                    </span>
-                                </div>
-                            ))}
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-between items-end">
-                {!!document.icon ? (
-                    <p className="text-2xl">{document.icon}</p>
-                ) : (
-                    <>
-                        <Image
-                            src="/logo.svg"
-                            height="20"
-                            width="20"
-                            alt="Empty"
-                            className="dark:hidden"
-                        />
-                        <Image
-                            src="/logo-dark.svg"
-                            height="20"
-                            width="20"
-                            alt="Empty"
-                            className="dark:block hidden"
-                        />
-                    </>
-                )}
-                <div className="flex flex-col items-end justify-end text-[9px]">
-                    <p>Created at:</p>
-                    <span className="font-semibold italic">
-                        {useDate(creationDate)}
-                    </span>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div
+			className={cn(
+				"w-full flex justify-between bg-gray-50 dark:bg-muted rounded-lg px-5 shadow-md relative overflow-hidden",
+				document.tags.length > 0 ? "py-4" : "h-36 py-4",
+				isMobile && "mb-10"
+			)}
+			style={{
+				backgroundImage: document.coverImage
+					? `url(${document.coverImage})`
+					: undefined,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				backgroundRepeat: "no-repeat",
+			}}
+		>
+			{document.coverImage && (
+				<div className="absolute inset-0 bg-white/80 dark:bg-gray-900/80 rounded-lg" />
+			)}
+			<div className="relative z-10 w-full flex justify-between">
+				<div className="flex flex-col w-[60%] justify-center">
+					<h2 className="text-xs text-muted-foreground mb-0.5">
+						By:{" "}
+						<Link href={`/user/${document.userId}`}>
+							<span className="font-semibold italic truncate">
+								{document.userName ? document.userName : "Jotion User"}
+							</span>
+						</Link>
+					</h2>
+					<h1
+						role="button"
+						className="text-2xl font-semibold truncate"
+						onClick={() => router.push(`/preview/${document._id}`)}
+					>
+						{document.title}
+					</h1>
+					{document.tags.length > 0 && (
+						<div className="flex flex-wrap items-center gap-1 mt-2">
+							{document.tags.length > 0 &&
+								document.tags.map((tag) => (
+									<div
+										key={tag}
+										className="p-1 border border-muted-foreground text-xs font-medium text-muted-foreground flex items-center justify-center rounded-sm max-w-xs"
+									>
+										<span className="truncate">
+											{tag.charAt(0).toUpperCase() + tag.slice(1)}
+										</span>
+									</div>
+								))}
+						</div>
+					)}
+				</div>
+				<div className="flex flex-col justify-between items-end">
+					{!!document.icon ? (
+						<p className="text-2xl">{document.icon}</p>
+					) : (
+						<>
+							<Image
+								src="/logo.svg"
+								height="20"
+								width="20"
+								alt="Empty"
+								className="dark:hidden"
+							/>
+							<Image
+								src="/logo-dark.svg"
+								height="20"
+								width="20"
+								alt="Empty"
+								className="dark:block hidden"
+							/>
+						</>
+					)}
+					<div className="flex flex-col items-end justify-end text-[9px]">
+						<p>Created at:</p>
+						<span className="font-semibold italic">
+							{useDate(creationDate)}
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-Article.Skeleton = function ArticleSkeleton () {
-    return (
-        <Skeleton className="w-full h-28" />
-    )
-}
+Article.Skeleton = function ArticleSkeleton() {
+	return <Skeleton className="w-full h-28" />;
+};
